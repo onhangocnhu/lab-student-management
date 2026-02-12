d5fe2d32d8827a10f3eccc7e855f4122f1facf7e
onhangocnhu <nhu.onhangoc@hcmut.edu.vn>
2026-02-12 17:25:51 +0700

    fix: student search filter

--- src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/repository/StudentRepository.java
+++ src/main/java/vn/edu/hcmut/cse/adsoftweng/lab/repository/StudentRepository.java
@@ -3,11 +3,17 @@ package vn.edu.hcmut.cse.adsoftweng.lab.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import vn.edu.hcmut.cse.adsoftweng.lab.entity.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, String> {
-  List<Student> findByNameContaining(String name);
+  @Query(
+    value = "SELECT * FROM students WHERE UNACCENT(LOWER(name)) LIKE UNACCENT(LOWER(CONCAT('%', :name, '%')))", 
+    nativeQuery = true
+  )
+  List<Student> findByNameContaining(@Param("name") String name);
 }
